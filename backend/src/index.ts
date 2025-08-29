59220cbd2c2c0a8577e95dbff716cbcc1c524ab4
Rishabh Damle <dwrushabh@gmail.com>
2025-08-29 14:54:29 +0530

    boostrapping is done

---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+//you can inforce your codebase should not have any ts-ignores
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const app = express();
