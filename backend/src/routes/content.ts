4c9f45f9c60ce7ca8eb1bd9435c1c97212f5713b
Rishabh Damle <dwrushabh@gmail.com>
2025-09-02 17:25:06 +0530

    sucssesfully implemented the backend feture of adding your content

--- backend/src/routes/content.ts
+++ backend/src/routes/content.ts
@@ -1,7 +1,23 @@
 import express from "express";
 import { Router } from "express";
 import { userAuth } from "../middlewares/userAuth.js";
+import { ContentModel } from "../db.js";
+import { TagModel } from "../db.js";
 export const contentRouter = Router();
 contentRouter.use(express.json());
 
-contentRouter.post("/createContent", userAuth, async (req, res) => {});
+contentRouter.post("/createContent", userAuth, async (req, res) => {
+  const userId = req.userId;
+  const { link, type, title, tag } = req.body;
+  const content = await ContentModel.create({
+    link: link,
+    type: type,
+    title: title,
+    userId: userId,
+  });
+
+  res.status(200).json({
+    Message: `Your content is added succsesfully`,
+    contentId: content._id,
+  });
+});
