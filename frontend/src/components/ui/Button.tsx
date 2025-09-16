c563802879d032e6f9617b87d0a3381be0923bdf
Rishabh Damle <dwrushabh@gmail.com>
2025-09-16 13:38:35 +0530

    Created a generic button component/Button UI Library

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -14,9 +14,20 @@ const buttonVariants = {
   primary: "bg-purple-600 text-white",
   secondary: "bg-purple-200 text-purple-600",
 };
-
+const defaultStyles = "rounded-md";
+const sizeStyles = {
+  sm: "py-1 px-2",
+  md: "py-2 px-4",
+  lg: "py-4 px-6",
+};
 export const Button = (props: ButtonProps) => {
   return (
-    <button className={buttonVariants[props.variant]}>{props.text}</button>
+    <button
+      className={`${buttonVariants[props.variant]} ${defaultStyles} ${
+        sizeStyles[props.size]
+      }`}
+    >
+      {props.text}
+    </button>
   );
 };
