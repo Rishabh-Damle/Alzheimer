5d7868d346e6606f0accab3846114ffb05f973c5
Vrushabh Damle <dwrushabh@gmail.com>
2025-09-27 17:28:36 +0530

    implement the feature of sharable link in the backend

--- backend/src/routes/brain.ts
+++ backend/src/routes/brain.ts
@@ -1,19 +1,32 @@
 import express from "express";
 import { Router } from "express";
 import { userAuth } from "../middlewares/userAuth.js";
-import { ContentModel, LinkModel } from "../db.js";
+import { ContentModel, LinkModel, UserModel } from "../db.js";
 import { random } from "../utils.js";
 export const brainRouter = Router();
 brainRouter.use(express.json());
 brainRouter.post("/share", userAuth, async (req, res) => {
   const { share } = req.body;
   if (share) {
+    //check whether the sharable link already exists or not
+    const existingLink = await LinkModel.findOne({
+      userId: req.userId,
+    });
+    if (existingLink) {
+      res.status(409).json({
+        error: "Sharable link already exists",
+        link: existingLink.hash,
+      });
+      return;
+    }
+    const hash = random(20);
     await LinkModel.create({
       userId: req.userId,
-      hash: random(10),
+      hash: hash,
     });
     res.status(201).json({
       message: "Your sharable link is successfully created.",
+      link: hash,
     });
   } else {
     await LinkModel.deleteOne({
@@ -22,6 +35,7 @@ brainRouter.post("/share", userAuth, async (req, res) => {
     res.status(200).json({
       message: "Your link delete request was successful.",
     });
+    return;
   }
 });
 
@@ -38,5 +52,15 @@ brainRouter.get("/share/:shareLink", async (req, res) => {
   }
   const content = await ContentModel.find({
     userId: link.userId,
+  });
+
+  const user = await UserModel.findOne({
+    _id: link.userId,
+  });
+
+  res.status(200).json({
+    message: "Data fetched successfully",
+    username: user?.username,
+    content: content,
   });
 });
