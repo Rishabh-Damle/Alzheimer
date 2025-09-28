b2f7f8d434aafec9e55934616998033cbcbaf7f5
Vrushabh Damle <dwrushabh@gmail.com>
2025-09-28 13:38:54 +0530

    modified:   frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,9 @@
 import "./App.css";
-import { Button } from "./components/ui/Button";
-import { PlusIcon } from "./components/icons/PlusIcon";
-import { ShareIcon } from "./components/icons/ShareIcon";
+
 function App() {
   return (
-    <div className="flex justify-center align-middle ">
-      <div className="mt-10 mx-2">
-        <Button
-          variant="primary"
-          text="Share"
-          size="md"
-          startIcon={<PlusIcon size="lg"></PlusIcon>}
-        ></Button>
-      </div>
-
-      <div className="mt-10 mx-2">
-        <Button
-          variant="secondary"
-          text="Add content"
-          size="md"
-          startIcon={<ShareIcon size="lg"></ShareIcon>}
-        ></Button>
-      </div>
-    </div>
+    
+      <div className="bg-red-500 ">hi there</div>
   );
 }
 
