8570c84386797a7ea5668ed42edec7d6d541bc14
Vrushabh Damle <dwrushabh@gmail.com>
2025-09-28 15:51:55 +0530

    Button component is nearly sorted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import { SwitchButton } from "./components/SwitchButton";
+import { PlusIcon } from "./components/icons/PlusIcon";
+import { ShareIcon } from "./components/icons/ShareIcon";
 function App() {
   return (
     
-      <div className="bg-red-500 ">hi there</div>
+      <div><SwitchButton variant="secondary" text="Share Brain" startIcon={<ShareIcon size="md"></ShareIcon>}></SwitchButton>
+      <SwitchButton variant="primary" text="Add Content" startIcon={<PlusIcon size="md"></PlusIcon>}></SwitchButton></div>
   );
 }
 
