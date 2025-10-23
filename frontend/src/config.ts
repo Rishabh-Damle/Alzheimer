7f54d78eebb2e4d6eb114d5b918d9bc860349a5b
Vrushabh Damle <dwrushabh@gmail.com>
2025-10-23 10:13:27 +0530

    Added the cors configurations

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://alzheimer-backend.vercel.app";
+
 export const SITE_URL = `${window.location.protocol}//${window.location.host}`;
