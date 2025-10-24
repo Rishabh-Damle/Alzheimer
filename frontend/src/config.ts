3bd5ed894b063231cb130dbb0f3c500c1295fd8f
Vrushabh Damle <dwrushabh@gmail.com>
2025-10-24 19:24:06 +0530

    error fix

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-export const BACKEND_URL = process.env.BACKEND_URL as string;
+export const BACKEND_URL = "https://alzheimer-backend.vercel.app";
+
 export const SITE_URL = `${window.location.protocol}//${window.location.host}`;
